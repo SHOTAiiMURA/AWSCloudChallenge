50fa2eb097966213866e90ede3cbaaf8d8694ba9
Hansolo 99 <tuk91581@temple.edu>
2025-01-27 03:11:30 +0000

    changed codes

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -1,6 +1,7 @@
 import './HomeFeedPage.css';
 import React from "react";
 
+import {Auth} from 'aws-amplify'
 import DesktopNavigation  from '../components/DesktopNavigation';
 import DesktopSidebar     from '../components/DesktopSidebar';
 import ActivityFeed from '../components/ActivityFeed';
@@ -36,15 +37,20 @@ export default function HomeFeedPage() {
   };
 
   const checkAuth = async () => {
-    console.log('checkAuth')
-    // [TODO] Authenication
-    if (Cookies.get('user.logged_in')) {
+    Auth.currentAuthenticatedUser({
+      bypassCache: false
+    })
+    .then((user)=>{
+      console.log('user', user);
+      return Auth.currentAuthenticatedUser()
+    }).then((cognito_user) => {
       setUser({
-        display_name: Cookies.get('user.name'),
-        handle: Cookies.get('user.username')
+        display_name: cognito_user.attributes.name,
+        handle: cognito_user.attributes.preferred_username
       })
-    }
-  };
+    })
+    .catch((err)=> console.log(err));
+  }; 
 
   React.useEffect(()=>{
     //prevents double call
